f3620987290d54491c16cec5d65180b1d59a387a
Antonio Afonso <tmsimk@gmail.com>
2024-03-28 10:56:47 +0000

    task #3 - Create a service to return joke string from class

---
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokesServiceImpl.java
@@ -0,0 +1,19 @@
+package guru.springframework.spring5jokesappv2.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokesServiceImpl implements JokesService {
+
+    ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokesServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    }
+
+    @Override
+    public String Joke() {
+        return null;
+    }
+}
